d1884fd7425b64b7a6c6eb85ed7334a736a238ec
PabloTP87 <pablo.tp87@gmail.com>
2020-09-15 01:22:46 -0300

    Finalizada lógica de authPhone y registro en Firebase

--- app/src/main/java/com/kona/phoneauth/CodeNumberActivity.kt
+++ app/src/main/java/com/kona/phoneauth/CodeNumberActivity.kt
@@ -1,11 +1,73 @@
 package com.kona.phoneauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 
 class CodeNumberActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var loadBar: ProgressBar
+    //private var mVerificationId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code_number)
+
+        val codeTxt = findViewById<EditText>(R.id.codeTxt)
+        val codeBtn = findViewById<Button>(R.id.codeSend)
+        loadBar = findViewById(R.id.load_bar_code)
+
+        val mVerificationId = intent.getStringExtra("AuthCredential")
+
+        mAuth = FirebaseAuth.getInstance()
+
+        codeBtn.setOnClickListener {
+            val code = codeTxt.text.toString()
+            if (code.isEmpty()){
+                Toast.makeText(this,"Favor ingrese el codigo de verificación", Toast.LENGTH_SHORT).show()
+            } else {
+                loadBar.visibility = View.VISIBLE
+
+                val credential = PhoneAuthProvider.getCredential(mVerificationId!!,code)
+                signInWithPhoneAuthCredential(credential)
+            }
+        }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential){
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                sendHome()
+            } else if (task.exception is FirebaseAuthInvalidCredentialsException){
+                Toast.makeText(this,"Código invalido", Toast.LENGTH_SHORT).show()
+            }
+            loadBar.visibility = View.GONE
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (mAuth.currentUser != null){
+            sendHome()
+        }
+    }
+
+    private fun sendHome(){
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/kona/phoneauth/MainActivity.kt
+++ app/src/main/java/com/kona/phoneauth/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.kona.phoneauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val signOutBtn = findViewById<Button>(R.id.logout)
+        val userNumber = findViewById<TextView>(R.id.phone_number_txt)
+        val idUser = findViewById<TextView>(R.id.uid_txt)
+
+        val numberPhone = "Numero: " + mAuth.currentUser!!.phoneNumber
+        val uid = "UID: " + mAuth.uid
+        userNumber.text = numberPhone
+        idUser.text = uid
+
+        signOutBtn.setOnClickListener {
+
+            mAuth.signOut()
+            sendLogin()
+        }
+
     }
+
+    private fun sendLogin() {
+        val intent = Intent(this,PhoneNumberActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        finish()
+    }
+
 }
